eadb18ab6d0cec067b6a7391a2b5a707ae3f6814
bigblacktomcat <master@blacktomcat.ru>
2020-07-23 13:38:47 +0300

    Add files via upload

--- Inc/stm32f1xx_it.h
+++ Inc/stm32f1xx_it.h
@@ -59,7 +59,7 @@ void SysTick_Handler(void);
 void DMA1_Channel1_IRQHandler(void);
 void USB_LP_CAN1_RX0_IRQHandler(void);
 /* USER CODE BEGIN EFP */
-
+void ADC_DMA_TransferComplete_Callback(void);
 /* USER CODE END EFP */
 
 #ifdef __cplusplus
